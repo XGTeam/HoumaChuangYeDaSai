107afa0c6e860862cc3110c19ee6e98569cf16f7
zgs225 <zgs225@gmail.com>
2015-10-29 16:02:41 +0800

    修正编辑页面样式

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,43 +1,45 @@
 @extends('layouts.default')
 
 @section('title')
-Edit {{ $post->title }}
-@stop
-
-@section('top')
-<div class="page-header">
-<h1>Edit {{ $post->title }}</h1>
-</div>
+编辑{{ $post->title }}
 @stop
 
 @section('content')
-<div class="row">
-    <div class="col-xs-6">
-        <p class="lead">
-            Please edit the post:
-        </p>
-    </div>
-    <div class="col-xs-6">
-        <div class="pull-right">
-            <a class="btn btn-success" href="{!! URL::route('blog.posts.show', array('posts' => $post->id)) !!}"><i class="fa fa-file-text"></i> Show Post</a> <a class="btn btn-danger" href="#delete_post" data-toggle="modal" data-target="#delete_post"><i class="fa fa-times"></i> Delete Post</a>
+<section class="bg-light-gray">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-10 col-md-offset-1 col-xs-12">
+        <div class="box box-padding-bottom">
+          <div class="box-body">
+            <div class="page-header">
+              <h1>
+                编辑{{ $post->title }}
+                <div class="pull-right">
+                  <a class="btn btn-danger btn-flat" href="#delete_post" data-toggle="modal" data-target="#delete_post">
+                    删除新闻
+                  </a>
+                </div>
+              </h1>
+            </div>
+          </div>
+
+          <?php
+          $form = ['url' => URL::route('blog.posts.update', ['posts' => $post->id]),
+              '_method' => 'PATCH',
+              'method' => 'POST',
+              'button' => '保存',
+              'defaults' => [
+                  'title' => $post->title,
+                  'summary' => $post->summary,
+                  'body' => $post->body,
+          ], ];
+          ?>
+          @include('posts.form')
         </div>
+      </div>
     </div>
-</div>
-<hr>
-<div class="well">
-    <?php
-    $form = ['url' => URL::route('blog.posts.update', ['posts' => $post->id]),
-        '_method' => 'PATCH',
-        'method' => 'POST',
-        'button' => 'Save Post',
-        'defaults' => [
-            'title' => $post->title,
-            'summary' => $post->summary,
-            'body' => $post->body,
-    ], ];
-    ?>
-    @include('posts.form')
-</div>
+  </div>
+</section>
 @stop
 
 @section('bottom')
@@ -47,9 +49,9 @@ Edit {{ $post->title }}
 @stop
 
 @section('css')
-<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-markdown/2.8.0/css/bootstrap-markdown.min.css">
+<link rel="stylesheet" type="text/css" href="/assets/styles/bootstrap-markdown.min.css">
 @stop
 
 @section('js')
-<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-markdown/2.8.0/js/bootstrap-markdown.min.js"></script>
+<script type="text/javascript" src="/assets/scripts/bootstrap-markdown.js"></script>
 @stop
--- resources/views/posts/form.blade.php
+++ resources/views/posts/form.blade.php
@@ -5,7 +5,7 @@
 
     <div class="form-group{!! ($errors->has('title')) ? ' has-error' : '' !!}">
         <label class="col-md-2 col-sm-3 col-xs-10 control-label" for="title">标题</label>
-        <div class="col-lg-3 col-md-4 col-sm-5 col-xs-10">
+        <div class="col-lg-8 col-md-10 col-sm-9 col-xs-12">
             <input name="title" value="{!! Request::old('title', $form['defaults']['title']) !!}" type="text" class="form-control without-radius" placeholder="标题">
             {!! ($errors->has('title') ? $errors->first('title') : '') !!}
         </div>
@@ -13,7 +13,7 @@
 
     <div class="form-group{!! ($errors->has('summary')) ? ' has-error' : '' !!}">
         <label class="col-md-2 col-sm-3 col-xs-10 control-label" for="summary">摘要</label>
-        <div class="col-lg-3 col-md-4 col-sm-5 col-xs-10">
+        <div class="col-lg-8 col-md-10 col-sm-9 col-xs-12">
             <input name="summary" value="{!! Request::old('summary', $form['defaults']['summary']) !!}" type="text" class="form-control without-radius" placeholder="摘要">
             {!! ($errors->has('summary') ? $errors->first('summary') : '') !!}
         </div>
@@ -21,7 +21,7 @@
 
     <div class="form-group{!! ($errors->has('body')) ? ' has-error' : '' !!}">
         <label class="col-md-2 col-sm-3 col-xs-10 control-label" for="body">正文</label>
-        <div class="col-lg-6 col-md-8 col-sm-9 col-xs-12">
+        <div class="col-lg-8 col-md-10 col-sm-9 col-xs-12">
             <textarea name="body"
                       type="text"
                       class="form-control"
